2fc95289748cfa12cbc4e1e8594f103f92a00393
Gideon Kim <gideon.kim@gordon.edu>
2024-01-19 23:21:30 -0500

    Set up database and connect

---
+++ backend/controllers/income.js
@@ -0,0 +1,29 @@
+const IncomeSchema = require("../models/IncomeModel");
+
+exports.addIncome = async (req, res) => {
+  const { title, amount, category, description, date } = req.body;
+
+  const income = IncomeSchema({
+    title,
+    amount,
+    category,
+    description,
+    date,
+  });
+
+  try {
+    //validations
+    if (!title || !category || !description || !date) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (amount <= 0 || !amount === "number") {
+      return res
+        .status(400)
+        .json({ message: "Amount must be a positive number" });
+    }
+    await income.save();
+    res.status(200).json({ message: "Income has been successfully added!" });
+  } catch (error) {}
+
+  console.log(income);
+};
