7ec51bce31a9676cc689a1f237964fbbdfcacf1d
Gideon Kim <gideon.kim@gordon.edu>
2024-01-24 23:45:40 -0500

    Create various components for tabs for finance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,41 @@ import styled from "styled-components";
 import bg from "./img/bg.png";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
+import React, { useMemo, useState } from "react";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Income from "./Components/Incomes/Income";
+import Expenses from "./Components/Expenses/Expenses";
 
 function App() {
+  const [active, setActive] = useState(1);
+
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />;
+      case 2:
+        return <Dashboard />;
+      case 3:
+        return <Income />;
+      case 4:
+        return <Expenses />;
+      default:
+        return <Dashboard />;
+    }
+  };
+
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
+
   return (
     <AppStyled bg={bg} className="App">
-      <Orb />
-      <MainLayout></MainLayout>
+      {orbMemo}
+      <MainLayout>
+        <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
+      </MainLayout>
     </AppStyled>
   );
 }
@@ -16,6 +45,17 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${(props) => props.bg});
   position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `;
 
 export default App;
